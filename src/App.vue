67d16f32516fcafd6dc815d6b500b6e15ffa51c7
Fahri Akbaba <fahriakbaba@gmail.com>
2023-06-18 21:37:44 +0300

    Header component was created

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <img src="https://avatars.githubusercontent.com/u/6128107?s=280&v=4" alt="vue-photo">
+        <h6>Vue Movie App</h6>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 1.25rem;
+
+    img {
+        width: 45px;
+    }
+
+    h6 {
+        font-size: 1.05rem;
+    }
+}
+</style>
