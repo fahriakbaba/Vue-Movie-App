e46b47ed739e8d4165e7c347f052239357edc10e
Fahri Akbaba <fahriakbaba@gmail.com>
2023-06-19 06:28:59 +0300

    added search input to Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,12 @@
 <template>
     <header>
-        <img src="https://avatars.githubusercontent.com/u/6128107?s=280&v=4" alt="vue-photo">
-        <h6>Vue Movie App</h6>
+        <div class="logo">
+            <img src="https://avatars.githubusercontent.com/u/6128107?s=280&v=4" alt="vue-photo">
+            <h6>Vue Movie App</h6>
+        </div>
+        <div class="search">
+            <input type="text" />
+        </div>
     </header>
 </template>
 
@@ -15,22 +20,28 @@ export default {
 <style scoped lang="scss">
 header {
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
     padding: .75rem 1.5rem;
     box-shadow: .5px .5px 5px gainsboro;
 
-    img {
-        width: 45px;
-    }
+    .logo {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        
+        img {
+            width: 45px;
+        }
 
-    h6 {
-        font-size: 1.05rem;
-        color: green;
-        opacity: .75;
-        letter-spacing: .5px;
-        word-spacing: 1px;
-        margin-left: .5rem;
+        h6 {
+            font-size: 1.05rem;
+            color: darkgreen;
+            opacity: .75;
+            letter-spacing: .1px;
+            word-spacing: 1px;
+            margin-left: .5rem;
+        }
     }
 }
 </style>
